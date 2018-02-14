4a156974842d80594100be451825e77efc300739
sashman <sashman90@gmail.com>
2018-02-14 17:33:02 +0000

    Bumping version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxExRay.MixProject do
   def project do
     [
       app: :phx_ex_ray,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
